620f24020053e02fc76348c07a56ed45b044649e
bautoo <bdieguezpisani@alumno.huergo.edu.ar>
2026-08-03 11:14:16 -0300

    peladini benjaciniiii
    actualizacion 11:15

--- refugio.py
+++ refugio.py
@@ -1,3 +1,16 @@
 import time, datos
 
+def registrar_adoptante():
+    adoptante = []
+    while False:
+        try:
+            nom = input("ingrese su nombre: ")
+            if nom.isalpha():
+                nom.list()
+                dni = int(input("ingrese su DNI: "))
+                telephone = int (input("ingrese su numero de telefono: "))
+                localidad = int(input("ingrese su localidad: "))
+        except Exception:
+            False
+
 def agregar_animal():
