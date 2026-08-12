fef0765674045e56f8d10c3c050ac0366241e891
BAUTOO <bdieguezpisani@alumno.huergo.edu.ar>
2026-08-12 11:08:00 -0300

    dccxbxcv
    xcvxcvcvxvxc

--- refugio.py
+++ refugio.py
@@ -1,16 +1,23 @@
-import time, datos
+import datos, time
 
 def registrar_adoptante():
+    adoptantes = []
     adoptante = []
-    while False:
+    a = False
+    while a == False:
         try:
-            nom = input("ingrese su nombre: ")
-            if nom.isalpha():
-                nom.list()
-                dni = int(input("ingrese su DNI: "))
-                telephone = int (input("ingrese su numero de telefono: "))
-                localidad = int(input("ingrese su localidad: "))
+            nom = input("Ingrese su nombre: ")
+            dni = input("Ingrese su DNI: ")
+            telefono = input("Ingrese su numero de telefono: ")
+            localidad = input("Ingrese su localidad: ")
+            if nom.isalpha() and dni.isdigit() and telefono.isdigit() and localidad.isalpha:
+                adoptante = ",".join[nom, dni, telefono, localidad]
+                adoptantes.append(adoptante)
+                return adoptantes, True
+            else:
+                False
         except Exception:
             False
 
 def agregar_animal():
+    adoptantes = registrar_adoptante()
